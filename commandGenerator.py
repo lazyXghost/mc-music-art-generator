c713d905de1d011585189472225c5f359335795a
lazyXghost <rajatbansal2702@gmail.com>
2024-04-13 16:08:12 +0530

    Removed code errors

--- commandGenerator.py
+++ commandGenerator.py
@@ -1,21 +1,16 @@
 import pickle as pkl
 import math
+import json
 from rajatsLibrary.minecraft import commandGenerator
 import configparser
+import argparse
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 config = config["MinecraftSettings"]
 
-music_box_dict = config["music_box_dict"]
-startingCoordinates = config["starting_coordinates"]
-startingX, startingY, startingZ = startingCoordinates[0], startingCoordinates[1], startingCoordinates[2]
-hearable_range = config["hearable_range"]
-one_floor_height = config["one_floor_height"]
-oneHundredMilliGapHorizontal = 3
-
-def getBlockDetails():
-    pickle_file_path = "Results/TetrisBeats_max.pkl"
+def getBlockDetails(results_path, target_file, music_box_dict):
+    pickle_file_path = f"{results_path}{target_file}.pkl"
     with open(pickle_file_path, "rb") as f:
         data = pkl.load(f)
 
@@ -41,31 +36,48 @@ def getBlockDetails():
         res.append((tm_value, curr_instruments_details))
     return res
 
-myCommandGenerator = commandGenerator()
+def main(results_path, target_file, music_box_dict, hearable_range, one_hundred_milli_horizontal_gap, startingCoordinates, one_floor_vertical_gap,):
+    startingX, startingY, startingZ = startingCoordinates[0], startingCoordinates[1], startingCoordinates[2]
+    myCommandGenerator = commandGenerator()
+    block_details = getBlockDetails(results_path, target_file, music_box_dict)
+    batch_size = int(hearable_range / one_hundred_milli_horizontal_gap)
+    floor_level = 0
+    output = myCommandGenerator.getCleanSpace(startingCoordinates, hearable_range, hearable_range, hearable_range)
+    x = startingX
+    for i in range(0, len(block_details), batch_size):
+        batch = block_details[i : i + batch_size]
+        inc = 1 if floor_level%2 == 0 else -1
+        for oneMilliNotes in batch:
+            tm = oneMilliNotes[0]
+            note_blocks = oneMilliNotes[1]
+            current_height = startingY + floor_level * one_floor_vertical_gap
+            output += myCommandGenerator.getMainRepeaterAndRedstoneLine((x, current_height, startingZ), tm, inc) +  myCommandGenerator.getInstantRepeater((x + inc, current_height, startingZ+15)) + myCommandGenerator.getInstantRepeater((x + inc, current_height, startingZ+33))
+            for note_block in note_blocks:
+                output += myCommandGenerator.getNoteBlock((x + 2 * inc, current_height, startingZ), inc, note_block)
+            x += 3 * inc
+        output += myCommandGenerator.getUpperFloorConnection((x, current_height, startingZ), inc)
+        x += inc
+        floor_level += 1
+    return output
 
-block_details = getBlockDetails()
-batch_size = int(hearable_range / oneHundredMilliGapHorizontal)
-floor_levels_required = math.ceil(len(block_details) / batch_size)
-floor_level = 0
-output = myCommandGenerator.getCleanSpace(startingCoordinates, hearable_range)
-x = startingX
-for i in range(0, len(block_details), batch_size):
-    batch = block_details[i : i + batch_size]
-    inc = 1 if floor_level%2 == 0 else -1
-    for oneMilliNotes in batch:
-        tm = oneMilliNotes[0]
-        note_blocks = oneMilliNotes[1]
-        current_height = startingY + floor_level * one_floor_height
-        output += myCommandGenerator.getMainRepeaterAndRedstoneLine((x, current_height, startingZ), tm, inc) +  myCommandGenerator.getInstantRepeater((x + inc, current_height, startingZ+15)) + myCommandGenerator.getInstantRepeater((x + inc, current_height, startingZ+33))
-        for note_block in note_blocks:
-            output += myCommandGenerator.getNoteBlock((x + 2 * inc, current_height, startingZ), inc, note_block)
-        x += 3 * inc
-    output += myCommandGenerator.getUpperFloorConnection((x, current_height, startingZ), inc)
-    x += inc
-    floor_level += 1
 
-with open("musicCommand.txt", "w") as f:
-    f.write(output)
+music_box_dict = json.loads(config["music_box_dict"])
+startingCoordinates = [int(_) for _ in config["starting_coordinates"].split(',')]
 
-# plt.scatter([i for i in hearable_range(len(asfValues))], asfValues)
-# plt.show()
+hearable_range = int(config["hearable_range"])
+one_floor_vertical_gap = int(config["one_floor_vertical_gap"])
+one_hundred_milli_horizontal_gap = int(config["100ms_horizontal_gap"])
+results_path = config["results_path"]
+
+parser = argparse.ArgumentParser(description="Command generator for minecraft note blocks")
+parser.add_argument("-f", "--file", help="Specify the file name for processing")
+args = parser.parse_args()
+
+if __name__ == "__main__":
+    target_file = args.file
+    commands = main(results_path, target_file, music_box_dict, hearable_range, one_hundred_milli_horizontal_gap, startingCoordinates, one_floor_vertical_gap)
+    with open(f"{results_path}musicCommand_{target_file}.txt", "w") as f:
+        f.write(commands)
+
+    # plt.scatter([i for i in range(len(asfValues))], asfValues)
+    # plt.show()
--- rajatsLibrary/minecraft.py
+++ rajatsLibrary/minecraft.py
@@ -9,14 +9,14 @@ class commandGenerator:
     def getInstantRepeater(coordinates):
         x, y, z = coordinates[0], coordinates[1], coordinates[2]
         res = f"""
-        #---------INSTANT_REPEATER-----------
-        setblock {x} {y} {z} dirt
-        setblock {x} {y + 1} {z} sticky_piston[facing=south]
-        setblock {x} {y + 1} {z + 1} redstone_block
-        setblock {x} {y} {z + 2} sticky_piston[facing=north,extended=true]
-        setblock {x} {y} {z + 3} dirt
-        setblock {x} {y + 1} {z + 3} redstone_wire
-        #------------------------------------
+#---------INSTANT_REPEATER-----------
+setblock {x} {y} {z} dirt
+setblock {x} {y + 1} {z} sticky_piston[facing=south]
+setblock {x} {y + 1} {z + 1} redstone_block
+setblock {x} {y} {z + 2} sticky_piston[facing=north,extended=true]
+setblock {x} {y} {z + 3} dirt
+setblock {x} {y + 1} {z + 3} redstone_wire
+#------------------------------------
         """
         return res
 
@@ -33,13 +33,13 @@ class commandGenerator:
             else pos
         )
         res = f"""
-        #--------------NOTE_BLOCK-{x}-------------------
-        setblock {x} {y-1} {z + pos} glass
-        setblock {x} {y} {z + pos} redstone_wire
-        setblock {x + inc} {y - 2} {z + pos} glass
-        setblock {x + inc} {y - 1} {z + pos} {block['block_name']}
-        setblock {x + inc} {y} {z + pos} note_block[note={block['note']}]
-        #-----------------------------------------------
+#--------------NOTE_BLOCK-{pos}-------------------
+setblock {x} {y-1} {z + pos} glass
+setblock {x} {y} {z + pos} redstone_wire
+setblock {x + inc} {y - 2} {z + pos} glass
+setblock {x + inc} {y - 1} {z + pos} {block['block_name']}
+setblock {x + inc} {y} {z + pos} note_block[note={block['note']}]
+#-----------------------------------------------
         """
         return res
 
@@ -47,15 +47,15 @@ class commandGenerator:
     def getMainRepeaterAndRedstoneLine(coordinates, tm, inc):
         x, y, z = coordinates[0], coordinates[1], coordinates[2]
         res = f"""
-        #------------------------------------{tm / 1000}-----------------------------------------
-        setblock {x} {y - 1} {z} glass 
-        setblock {x} {y} {z} repeater[delay=1,facing=west]
+#------------------------------------{tm / 1000}-----------------------------------------
+setblock {x} {y - 1} {z} glass 
+setblock {x} {y} {z} repeater[delay=1,facing=west]
 
-        fill {x + inc} {y - 1} {z} {x + inc} {y - 1} {z + 48} glass 
-        fill {x + inc} {y} {z} {x + inc} {y} {z + 48} redstone_wire
+fill {x + inc} {y - 1} {z} {x + inc} {y - 1} {z + 48} glass 
+fill {x + inc} {y} {z} {x + inc} {y} {z + 48} redstone_wire
 
-        setblock {x + 2*inc} {y - 1} {z} glass
-        setblock {x + 2*inc} {y} {z} redstone_wire
+setblock {x + 2*inc} {y - 1} {z} glass
+setblock {x + 2*inc} {y} {z} redstone_wire
         """
         return res
 
@@ -63,18 +63,22 @@ class commandGenerator:
     def getUpperFloorConnection(coordinates, inc):
         x, y, z = coordinates[0], coordinates[1], coordinates[2]
         res = f"""
-        #--------------UPPER-FLOOR-CONNECTION-------------------
-        setblock {x} {y} {z} redstone_wire
-        setblock {x} {y} {z} redstone_wire
-        setblock {x} {y} {z - 1} glass
-        setblock {x} {y + 1} {z - 1} redstone_wire
-        setblock {x} {y + 1} {z - 2} glass
-        setblock {x} {y + 2} {z - 2} redstone_wire
-        setblock {x + inc} {y + 2} {z - 2} glass
-        setblock {x + inc} {y + 3} {z - 2} redstone_wire
-        setblock {x + inc} {y + 3} {z - 1} glass
-        setblock {x + inc} {y + 4} {z - 1} redstone_wire
-        setblock {x + inc} {y + 4} {z} redstone_wire
-        #-------------------------------------------------------
+#--------------UPPER-FLOOR-CONNECTION-------------------
+setblock {x} {y} {z} redstone_wire
+setblock {x} {y} {z} redstone_wire
+setblock {x} {y} {z - 1} glass
+setblock {x} {y + 1} {z - 1} redstone_wire
+setblock {x} {y + 1} {z - 2} glass
+setblock {x} {y + 2} {z - 2} redstone_wire
+setblock {x + inc} {y + 2} {z - 2} glass
+setblock {x + inc} {y + 3} {z - 2} redstone_wire
+setblock {x + inc} {y + 3} {z - 1} glass
+setblock {x + inc} {y + 4} {z - 1} redstone_wire
+setblock {x + inc} {y + 4} {z} redstone_wire
+#-------------------------------------------------------
         """
         return res
+
+if __name__ == "__main__":
+    print("This is a library for Audio Manipulation via fourier transform made specificaly for minecraft audio production using note blocks")
+    print("Author -: Rajat Bansal, IIT Mandi, B20123")
